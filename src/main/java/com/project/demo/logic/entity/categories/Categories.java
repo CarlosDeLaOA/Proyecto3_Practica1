6608073b0c8fc07aff32523c178cf08e179667e6
CarlosDeLaOA <cdelaoa@ucenfotec.ac.cr>
2025-10-03 12:03:59 -0600

    Cambios finales

--- src/main/java/com/project/demo/logic/entity/categories/Categories.java
+++ src/main/java/com/project/demo/logic/entity/categories/Categories.java
@@ -13,7 +13,6 @@ public class Categories {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(nullable = false, length = 100)
     private String name;
 
